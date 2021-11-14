1b5c0a4414abd958604ab74bc6d44e5e3def4636
archana vinayagam <saiarchana.93@gmail.com>
2021-11-14 11:30:39 +0530

    01sum testcase4 passed

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(num1 , num2=0) {
+function sum(num1=0 , num2=0) {
   return num1 + num2;
 }
 
@@ -29,11 +29,13 @@ try {
   // When invoked with only 1 number, it should return that number.
   var result = sum(1);
   if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
-  
+
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
